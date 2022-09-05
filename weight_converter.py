391f76470a608a7137f5aadf0a97922e8947910c
chick_hicks42 <101632707+BreakfastnBread@users.noreply.github.com>
2022-09-05 21:25:07 +1000

    Add files via upload

---
+++ weight_converter.py
@@ -0,0 +1,9 @@
+#basic progran that converts weight from Kgs to Lbs, vice versa.
+weight = float(input("Input your weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "K":
+    converted = weight / 0.45
+    print("Weight in Lbs: " + str(converted))
+else:
+    converted = weight * 0.45
+    print("Weight in Kgs: " + str(converted))
